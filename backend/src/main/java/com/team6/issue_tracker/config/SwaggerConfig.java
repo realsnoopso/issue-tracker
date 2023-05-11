92dc2a484f4828d0fdc07f1ab107e5ea0049b0f2
saehim <saeilee.vxv@gmail.com>
2023-05-11 16:10:11 +0900

    Feat : SwaggerConfig 구현
    - 그룹 이름 team6
    - 모든 api 추가
    - 제목, 설명, 버전 추가

---
+++ backend/src/main/java/com/team6/issue_tracker/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.team6.issue_tracker.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("team6")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Issue Tracker API")
+                        .description("Issue Tracker API 명세서입니다.")
+                        .version("v0.0.1"));
+    }
+}
