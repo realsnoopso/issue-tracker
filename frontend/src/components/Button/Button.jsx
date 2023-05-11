80d509b50eeaa4c9a5ca4d05f263c7a476370384
sarang_daddy <sarang_daddy@naver.com>
2023-05-11 15:56:24 +0900

    Feat : Button 컴포넌트 추가

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,56 @@
+import style from './Button.module.css';
+
+export function Button(props) {
+	const { icon, text, type, status, color, btnSize, _onClick } = props;
+	const {
+		BtnStyle,
+		BtnColorBlue,
+		BtnOutlineColorBlue,
+		BtnColorBlack,
+		BtnOutlineColorBlack,
+		BtnSizeL,
+		BtnSizeM,
+		BtnSizeS,
+		BtnStyleContained,
+		BtnStyleGhost,
+		BtnStatusDefault,
+		BtnStatusHover,
+		BtnStatusPress,
+		BtnStatusDisabled,
+		BtnStatusActive,
+	} = style;
+
+	const btnSizeKind =
+		btnSize === 'l' ? BtnSizeL : btnSize === 'm' ? BtnSizeM : BtnSizeS;
+
+	const btnStatus =
+		status === 'hover'
+			? BtnStatusHover
+			: status === 'press'
+			? BtnStatusPress
+			: status === 'disabled'
+			? BtnStatusDisabled
+			: status === 'active'
+			? BtnStatusActive
+			: BtnStatusDefault;
+
+	const btnType = type === 'ghost' ? BtnStyleGhost : BtnStyleContained;
+
+	const btnColor =
+		color === 'blue'
+			? type === 'outline'
+				? BtnOutlineColorBlue
+				: BtnColorBlue
+			: type === 'outline'
+			? BtnOutlineColorBlack
+			: BtnColorBlack;
+
+	const buttonClassNames = `${BtnStyle} ${btnColor} ${btnSizeKind} ${btnStatus} ${btnType}`;
+
+	return (
+		<button className={buttonClassNames} onClick={_onClick}>
+			{icon === '+' && `${icon} `}
+			{text}
+		</button>
+	);
+}
