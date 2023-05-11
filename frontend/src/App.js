34921261272ff4557e7b2145368174335bf47394
sarang_daddy <sarang_daddy@naver.com>
2023-05-11 17:32:01 +0900

    Feat : 이슈 페이지에 리스트 불러오기 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.module.css';
+import { IssuePage } from '@containers/IssuePage/IssuePage';
 
 function App() {
 	if (process.env.NODE_ENV === 'development') {
@@ -7,44 +7,9 @@ function App() {
 		worker.start();
 	}
 
-	async function fetchUsers() {
-		const response = await fetch('https://api.example.com/users');
-		const data = await response.json();
-		console.log(data);
-	}
-
-	async function createUser(user) {
-		const response = await fetch('https://api.example.com/users', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(user),
-		});
-		const data = await response.json();
-		return data;
-	}
-
-	fetchUsers();
-	createUser({ id: 3, name: 'snoop' });
-	fetchUsers();
-
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<IssuePage></IssuePage>
 		</div>
 	);
 }
