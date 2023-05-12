6a99e8bdcc211a7cf0fa45dfb12c5b4ad5b5c2e3
realsnoopso <real@snoop.so>
2023-05-12 11:39:52 +0900

    Style : Icon 코드 컨벤션 변경

--- frontend/src/components/Icon/Icon.jsx
+++ frontend/src/components/Icon/Icon.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import iconfiles from '@assets/icons/index';
 
-export function Icon({ name = 'smile', width = '16', height = '16' }) {
+export const Icon = ({ name = 'smile', width = '16', height = '16' }) => {
 	const IconComponent = iconfiles[name];
 	return <IconComponent width={width} height={height}></IconComponent>;
-}
+};
 
 export default Icon;
