bd5536f1a0e5415b9d94b400f695dc00ee204ec4
saehim <saeilee.vxv@gmail.com>
2023-05-12 09:59:16 +0900

    Test : S3 Image Hosting 을 위한 기능

---
+++ backend/src/main/java/com/team6/issue_tracker/cloud/util/MultifileToFileConvertor.java
@@ -0,0 +1,23 @@
+package com.team6.issue_tracker.cloud.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+public class MultifileToFileConvertor {
+
+    public static Optional<File> convert(MultipartFile multipartFile) throws IOException {
+        File file = new File("src/main/resources/store/" + multipartFile.getOriginalFilename());
+
+        if (file.createNewFile()) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+                fileOutputStream.write(multipartFile.getBytes());
+            }
+            return Optional.of(file);
+        }
+        return Optional.empty();
+    }
+}
