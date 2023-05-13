732a1c7391c2d3e02e9fe9dbc92566ae31a53b67
realsnoopso <real@snoop.so>
2023-05-13 17:52:26 +0900

    Add : alias 세팅

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = function override(config, env) {
 	config.resolve.alias = {
 		...config.resolve.alias,
+		'@src': path.resolve(__dirname, 'src'),
 		'@components': path.resolve(__dirname, 'src/components'),
 		'@containers': path.resolve(__dirname, 'src/containers'),
 		'@services': path.resolve(__dirname, 'src/services'),
@@ -10,6 +11,7 @@ module.exports = function override(config, env) {
 		'@assets': path.resolve(__dirname, 'src/assets'),
 		'@stores': path.resolve(__dirname, 'src/stores'),
 		'@constants': path.resolve(__dirname, 'src/constants'),
+		'@styles': path.resolve(__dirname, 'src/styles'),
 	};
 
 	return config;
