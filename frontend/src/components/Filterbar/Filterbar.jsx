b3a7699852b674787423ee57663ed4a27bd0af96
realsnoopso <real@snoop.so>
2023-05-15 20:46:46 +0900

    Design : TextInput 스타일 조정

--- frontend/src/components/Filterbar/Filterbar.jsx
+++ frontend/src/components/Filterbar/Filterbar.jsx
@@ -4,14 +4,18 @@ import styles from './Filterbar.module.css';
 import classNames from 'classnames/bind';
 import { useState } from 'react';
 import { TextInput } from '../TextInput/TextInput';
-import { options } from '@constants/filterbar';
 
-export const Filterbar = () => {
+export const Filterbar = ({ options }) => {
   const cx = classNames.bind(styles);
   const filterbarClassNames = cx('filterbar');
   const dropdownWidth = '128px';
   const dropdownText = '필터';
   const dropdownHeader = '이슈 헤더';
+  const textinputStyle = Object.freeze({
+    height: 'calc(40px - 2px)',
+    borderRadius: '0 11px 11px 0',
+    width: '472px',
+  });
 
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [selected, setSelected] = useState('필터');
@@ -36,7 +40,11 @@ export const Filterbar = () => {
           setSelected(currentTarget.innerText)
         }
       ></Dropdown>
-      <TextInput placeholder={placeholder}></TextInput>
+      <TextInput
+        placeholder={placeholder}
+        icon="search"
+        style={textinputStyle}
+      ></TextInput>
     </div>
   );
 };
--- frontend/src/components/TextInput/TextInput.jsx
+++ frontend/src/components/TextInput/TextInput.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './TextInput.module.css';
 import classNames from 'classnames/bind';
+import { Icon } from '@components/index';
 
 export const TextInput = ({
   size,
@@ -9,6 +10,8 @@ export const TextInput = ({
   id,
   width,
   placeholder = 'placehoder',
+  style,
+  icon,
 }) => {
   const [inputValue, setInputValue] = useState('');
 
@@ -22,18 +25,20 @@ export const TextInput = ({
   const textInputState = states === 'initial' ? cx('initial') : cx('error');
 
   const textInputClassName = `${cx(
-    'TextInput'
+    'textinput'
   )} ${textInputMode} ${textInputState}`;
-
   const LabelClassName = `${cx('typo-dropdown-header')} ${cx('label')}`;
-
   const InputCLassName = `${cx('typo-body')} ${cx('input')}`;
+  const placeholderColor = `var(--color-light-neutral-text-weak)`;
 
   return (
-    <div style={{ width }} className={textInputClassName}>
-      <label className={LabelClassName} htmlFor={id}>
-        {label}
-      </label>
+    <div style={{ width, ...style }} className={textInputClassName}>
+      {label && (
+        <label className={LabelClassName} htmlFor={id}>
+          {label}
+        </label>
+      )}
+      {icon && <Icon name={icon} fill={placeholderColor}></Icon>}
       <input
         className={InputCLassName}
         type="text"
