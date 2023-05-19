6dc29acfdc2ada21d968c42a58d6b0af0adad8b4
saehim <saeilee.vxv@gmail.com>
2023-05-19 17:59:03 +0900

    Feat : `member` entity 정의

--- backend/src/main/java/com/team6/issue_tracker/member/domain/Member.java
+++ backend/src/main/java/com/team6/issue_tracker/member/domain/Member.java
@@ -1,18 +1,34 @@
 package com.team6.issue_tracker.member.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
+@Table("member")
 public class Member {
-    private Long idx;
-    private String memberId;
+
+    @Id
+    private Long memberIdx;
+    private String id;
+    private String password;
+    private String profileImageUrl;
+    @Column("github_join")
     private boolean gitSync;
 
-    public Member(Long idx, String memberId, boolean gitSync) {
-        this.idx = idx;
-        this.memberId = memberId;
+    @PersistenceCreator
+    private Member(String id, String password, String profileImageUrl, boolean gitSync) {
+        this.id = id;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
         this.gitSync = gitSync;
+    }
+
+    public static Member newMember(String id, String password, String profileImageUrl) {
+        return new Member(id, password, profileImageUrl, false);
     }
 }
