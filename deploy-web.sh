6a5e8e71de9fd0f266a8baa78a7b811af7149d1c
lvalentine6 <ysr1227@naver.com>
2023-05-21 04:51:21 +0900

    Feat : 프런트 배포를 위한 deploy.yml 파일 변경 및 추가

---
+++ deploy-web.sh
@@ -0,0 +1,8 @@
+# Remove the default Nginx file
+rm -rf /usr/share/nginx/html/*
+
+# Move the build files to the Nginx directory
+mv /path/to/your/react/build/* /usr/share/nginx/html
+
+# Restart Nginx
+service nginx restart
