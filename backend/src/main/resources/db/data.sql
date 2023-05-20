1236292e4757330aa95cfdc8b851609e88e1c682
saehim <saeilee.vxv@gmail.com>
2023-05-21 02:18:13 +0900

    Feat : 기본 데이터 세팅

--- backend/src/main/resources/db/data.sql
+++ backend/src/main/resources/db/data.sql
@@ -4,3 +4,11 @@ INSERT `issue_tracker_schema`.member (member_idx, id, password, profile_image_ur
             (2, 'roy', '1234', '', false),
             (3, 'snoop', '1234', '', true),
             (4, 'sarangDaddy', '1234', '', false);
+
+# issue init
+INSERT `issue` (issue_idx, issue_title, issue_contents, issue_status, issue_created_at, issue_deleted, member_idx_writer, member_idx_assignee)
+VALUES (1, 'test issue', '테스트 이슈입니다.', 1, now(), 0, 1, 2);
+
+# comment init
+INSERT comment (comment_idx, contents, created_at, edited_at, state, issue_idx, created_by)
+VALUES (1, 'test comment', now(), null, 1, 1, 2);
