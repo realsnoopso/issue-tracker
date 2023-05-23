fea12f17022657b56517b9eaa1eee3bc49733054
realsnoopso <real@snoop.so>
2023-05-23 17:23:02 +0900

    Design : 로그인 페이지 스타일 변경

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,16 +1,12 @@
-import styles from './Layout.module.css';
-import classNames from 'classnames/bind';
 import { Navbar } from '@components/index';
 import { MY_USER_DATA } from '@src/constants/user';
 
-const cx = classNames.bind(styles);
-const contentsClassNames = cx('contents');
-
 export const Layout = ({ children, isLogin }) => {
+  const contentsInlineStyles = { marginTop: isLogin ? '32px' : '0px' };
   return (
     <>
       {isLogin && <Navbar user={MY_USER_DATA}></Navbar>}
-      <div className={contentsClassNames}>{children}</div>
+      <div style={contentsInlineStyles}>{children}</div>
     </>
   );
 };
--- frontend/src/containers/LoginPage/LoginPage.jsx
+++ frontend/src/containers/LoginPage/LoginPage.jsx
@@ -1,16 +1,63 @@
 import { Button } from '@components/index';
+import imagefiles from '@assets/images/index';
+import { TextInput } from '@src/components/TextInput/TextInput';
+import classNames from 'classnames/bind';
+import styles from './LoginPage.module.css';
+const cx = classNames.bind(styles);
 
 export const LoginPage = () => {
-  const handleClick = (event) => {
+  const LogoComponent = imagefiles['logoType'];
+  const loginPageClassNames = `${cx('login-page')}`;
+  const containerClassNames = `${cx('container')}`;
+
+  const handleClick = () => {
     const scope = 'user';
     const redirectUri = 'http://localhost:3000/auth';
     const clientId = '309fa9e4369279656330';
     window.location.href = `https://github.com/login/oauth/authorize?response_type=code&redirect_uri=${redirectUri}&client_id=${clientId}&scope=${scope}`;
   };
+  const logoHeight = 40;
+
+  const githubLoginProps = {
+    width: '100%',
+    text: 'GitHub 계정으로 로그인',
+  };
+
+  const idTextInputProps = {
+    placeholder: '아이디',
+    size: 'l',
+  };
+
+  const passwordTextInputProps = {
+    placeholder: '패스워드',
+    size: 'l',
+  };
+
+  const idLoginBtnProps = {
+    width: '100%',
+    color: 'blue',
+    text: '아이디로 로그인',
+  };
+
+  const registerBtnProps = {
+    width: '100%',
+    text: '회원가입',
+    type: 'ghost',
+    btnSize: 's',
+    iconName: 'plus',
+  };
 
   return (
-    <div>
-      <Button text={'login'} _onClick={handleClick}></Button>
+    <div className={loginPageClassNames}>
+      <LogoComponent height={logoHeight}></LogoComponent>
+      <div className={containerClassNames}>
+        <Button {...githubLoginProps} _onClick={handleClick}></Button>
+        <p>or</p>
+        <TextInput {...idTextInputProps}></TextInput>
+        <TextInput {...passwordTextInputProps}></TextInput>
+        <Button {...idLoginBtnProps}></Button>
+        <Button {...registerBtnProps}></Button>
+      </div>
     </div>
   );
 };
