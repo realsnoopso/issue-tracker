761a1b2c99bfda31d40ec1e4433d36ded9ab0a23
saehim <saeilee.vxv@gmail.com>
2023-05-23 17:02:16 +0900

    Feat : 인증인가를 위한 상수 정의

---
+++ backend/src/main/java/com/team6/issue_tracker/auth/config/AuthConst.java
@@ -0,0 +1,14 @@
+package com.team6.issue_tracker.auth.config;
+
+public class AuthConst {
+
+    public static final String ACCESS_TOKEN = "Access-Token";
+    public static final String REFRESH_TOKEN = "Refresh-Token";
+
+    public static final String BEARER = "Bearer";
+
+    public static final String USER_ID = "User_Id";
+    public static final String SOCIAL_ID = "Social_Id";
+    public static final String RESOURCE_SERVER = "Resource_Server";
+    public static final String TOKEN_TYPE = "Token_Type";
+}
