c833aece0ed2ac69fd22069ea55e2f4462ceae41
saehim <saeilee.vxv@gmail.com>
2023-05-24 13:49:29 +0900

    Feat : Github OAuth를 위한 bean 구현

--- backend/src/main/java/com/team6/issue_tracker/auth/config/GithubOAuthProperties.java
+++ backend/src/main/java/com/team6/issue_tracker/auth/config/GithubOAuthProperties.java
@@ -1,22 +1,19 @@
 package com.team6.issue_tracker.auth.config;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
-@Configuration
-@RequiredArgsConstructor
 public class GithubOAuthProperties {
 
-    @Value("${spring.security.oauth2.client.registration.github.clientId}")
-    private final String clientId;
+    String clientId;
 
-    @Value("${spring.security.oauth2.client.registration.github.clientSecret}")
-    private final String clientSecret;
+    String clientSecret;
 
-    @Value("${spring.security.oauth2.client.registration.github.redirectUri}")
-    private final String redirectUri;
+    String redirectUri;
 
+    public GithubOAuthProperties(String clientId, String clientSecret, String redirectUri) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+    }
 }
