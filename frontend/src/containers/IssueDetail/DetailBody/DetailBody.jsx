34ff1de3d73886541b215a7535b7e735a823ee25
sarang_daddy <sarang_daddy@naver.com>
2023-05-25 15:11:15 +0900

    Feat : 코멘트 컴포넌트 완성

--- frontend/src/containers/IssueDetail/DetailBody/DetailBody.jsx
+++ frontend/src/containers/IssueDetail/DetailBody/DetailBody.jsx
@@ -3,10 +3,35 @@ import classNames from 'classnames/bind';
 import { CommentElement } from '@containers/index';
 
 export const DetailBody = ({ issueObject }) => {
+  const commentList = issueObject.comment;
+  const issueWriterId = issueObject.writer?.id;
+
+  const addCommentElement = () => {
+    return commentList?.map((comment, index) => {
+      const commentWriterId = comment?.writer.id;
+      const writerProfile = comment?.writer.profile;
+      const writerName = comment?.writer.name;
+      const wroteTime = comment?.createdAt;
+      const contents = comment?.contents;
+
+      return (
+        <CommentElement
+          key={index}
+          issueWriterId={issueWriterId}
+          commentWriterId={commentWriterId}
+          writerProfile={writerProfile}
+          writerName={writerName}
+          wroteTime={wroteTime}
+          contents={contents}
+        ></CommentElement>
+      );
+    });
+  };
+
   return (
     <div className="detail-body">
       <div className="detail-comment">
-        <CommentElement issueObject={issueObject}></CommentElement>
+        {addCommentElement()}
         <div className="add-comment"></div>
       </div>
       <div className="detail-option"></div>
