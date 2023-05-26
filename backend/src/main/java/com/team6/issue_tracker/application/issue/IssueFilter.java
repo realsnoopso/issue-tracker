ce16cd6e927d165a1fc5307393f48517af1efa3e
saehim <saeilee.vxv@gmail.com>
2023-05-27 02:08:03 +0900

    Feat: `issueFilter` 구현
    - issue repository 조회 조건

---
+++ backend/src/main/java/com/team6/issue_tracker/application/issue/IssueFilter.java
@@ -0,0 +1,22 @@
+package com.team6.issue_tracker.application.issue;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class IssueFilter {
+
+    Boolean isOpen;
+    Integer page;
+    Long writer;
+    Long assignee;
+    List<Long> label;
+    Long mailestone;
+    List<Long> commentBy;
+
+}
