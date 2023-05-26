0a9021bf805c0962c8de0d0ea81464f066ebac68
saehim <saeilee.vxv@gmail.com>
2023-05-26 12:10:27 +0900

    Test : issue repository  기본 기능 단위 테스트
    - find all
    - 삭제되지 않고 열린 상태인 이슈 조회
    - 필터 조회

--- backend/src/test/java/com/team6/issue_tracker/issue/IssueRepositoryTest.java
+++ backend/src/test/java/com/team6/issue_tracker/issue/IssueRepositoryTest.java
@@ -1,7 +1,88 @@
 package com.team6.issue_tracker.issue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.team6.issue_tracker.application.issue.IssueRepository;
+import com.team6.issue_tracker.application.issue.domain.Issue;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class IssueRepositoryTest {
 
+    @Autowired
+    IssueRepository issueRepository;
+
+    @Test
+    @DisplayName("전체 이슈를 조회할 수 있다.")
+    public void findAll() throws Exception{
+        //when
+        Iterable<Issue> all = issueRepository.findAll();
+
+        //then
+        assertThat(all).hasSize(4);
+    }
+
+    @Test
+    @DisplayName("삭제되지 않고, 열린 상태인 이슈를 조회할 수 있다.")
+    public void findAllNotDeletedIsOpenIssues() throws Exception{
+        //given
+        PageRequest page = PageRequest.of(0,10, Sort.by(Sort.Direction.DESC, "issue_idx"));
+
+        //when
+        List<Issue> issues = issueRepository.findAllByIsDeletedAndIsOpen(false, true, page);
+
+        //then
+        assertThat(issues).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("삭제되지 않고, 열린 상태인 이슈를 페이지 크기 3인 첫번째 페이지를 조회할 수 있다.")
+    public void findAllNotDeletedIsOpenIssuesWithPage() throws Exception{
+        //given
+        PageRequest page = PageRequest.of(0,3, Sort.Direction.DESC, "issue_idx");
+
+        //when
+        List<Issue> issues = issueRepository.findAllByIsDeletedAndIsOpen(false, true, page);
+
+        //then
+        assertThat(issues).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("삭제되지 않고, 열린 상태인 이슈를 페이지 크기 1인 두번째 페이지를 조회할 수 있다.")
+    public void findAllNotDeletedIsOpenIssuesWithSecondPage() throws Exception{
+        //given
+        PageRequest page = PageRequest.of(1,1, Sort.by(Sort.Direction.DESC, "issue_idx"));
+
+        //when
+        List<Issue> issues = issueRepository.findAllByIsDeletedAndIsOpen(false, true, page);
+
+        //then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(issues).hasSize(1);
+            softly.assertThat(issues.get(0).getTitle()).isEqualTo("test issue");
+        });
+    }
+
+    @Test
+    @DisplayName("필터가 있는 경우, 조건에 맞는 issue를 조회할 수 있다.")
+    public void findWithFilter() throws Exception{
+        //when
+        List<Issue> allByFilter = issueRepository.findAllByFilter(true, null, 2L, null, null);
+
+        //then
+        assertThat(allByFilter).hasSize(2);
+    }
 }
