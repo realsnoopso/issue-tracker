0eec65154edba0ea84fa4f31772944e58005a89c
saehim <saeilee.vxv@gmail.com>
2023-05-27 01:50:35 +0900

    Feat: `IssueDto`, `IssueMapper` Milestone 추가

--- backend/src/main/java/com/team6/issue_tracker/application/issue/dto/IssueDto.java
+++ backend/src/main/java/com/team6/issue_tracker/application/issue/dto/IssueDto.java
@@ -5,8 +5,8 @@ import com.team6.issue_tracker.application.member.dto.MemberDto;
 import com.team6.issue_tracker.application.milestone.Milestone;
 import lombok.Builder;
 import lombok.Data;
-import org.joda.time.LocalDateTime;
 
+import java.time.Instant;
 import java.util.List;
 
 
@@ -18,7 +18,8 @@ public class IssueDto {
     private MemberDto writer;
     private MemberDto assignee;
     private Status status;
-    private LocalDateTime createdAt;
+    private Instant createdAt;
+    private Instant editedAt;
     //TODO milestone dto로 바꾸기
     private Milestone milestone;
     private List<LabelDto> labelList;
@@ -26,13 +27,14 @@ public class IssueDto {
     public IssueDto() {
     }
 
-    public IssueDto(Long index, String title, MemberDto writer, MemberDto assignee, Status status, LocalDateTime createdAt, Milestone milestone, List<LabelDto> labelList) {
+    public IssueDto(Long index, String title, MemberDto writer, MemberDto assignee, Status status, Instant createdAt, Instant edditedAt, Milestone milestone, List<LabelDto> labelList) {
         this.index = index;
         this.title = title;
         this.writer = writer;
         this.assignee = assignee;
         this.status = status;
         this.createdAt = createdAt;
+        this.editedAt = edditedAt;
         this.milestone = milestone;
         this.labelList = labelList;
     }
