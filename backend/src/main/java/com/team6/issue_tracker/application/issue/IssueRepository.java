7116aae2109682cb49ac363ed4a583c81a10f512
saehim <saeilee.vxv@gmail.com>
2023-05-26 12:07:47 +0900

    Feat : issue list 필터링 기능  추가

--- backend/src/main/java/com/team6/issue_tracker/application/issue/IssueRepository.java
+++ backend/src/main/java/com/team6/issue_tracker/application/issue/IssueRepository.java
@@ -1,9 +1,35 @@
 package com.team6.issue_tracker.application.issue;
 
 import com.team6.issue_tracker.application.issue.domain.Issue;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IssueRepository extends CrudRepository<Issue, Long> {
+public interface IssueRepository extends CrudRepository<Issue, Long>, PagingAndSortingRepository<Issue, Long> {
+
+    List<Issue> findAllByIsDeletedAndIsOpen(Boolean isDeleted, Boolean isOpen, Pageable pageable);
+
+    @Query(value = "SELECT i.* FROM issue i " +
+            "LEFT JOIN labeling il ON il.issue_idx = i.issue_idx " +
+            "WHERE (:isOpen IS NULL OR i.is_open = :isOpen) " +
+            "AND (:milestoneIdx IS NULL OR i.milestone_idx = :milestoneIdx) " +
+            "AND (:writer IS NULL OR i.writer = :writer) " +
+            "AND (:assignee IS NULL OR i.assignee = :assignee) " +
+            "AND (:labelIdx IS NULL OR il.label_idx IN (:labelIdx)) " +
+            "ORDER BY i.issue_idx DESC ")
+//            "LIMIT :pageSize OFFSET :offset")
+    List<Issue> findAllByFilter(@Param("isOpen") Boolean isOpen,
+                                @Param("milestoneIdx") Long milestoneIdx,
+                                @Param("writer") Long writer,
+                                @Param("assignee") Long assignee,
+                                @Param("labelIdx") List<Long> labelIdx);
+//                                @Param("pageSize") int pageSize,
+//                                @Param("offset") int offset);
+
 }
