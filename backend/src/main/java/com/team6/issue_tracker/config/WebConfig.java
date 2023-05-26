eb495c17e6e50adea98e417228204c778b4e5a04
saehim <saeilee.vxv@gmail.com>
2023-05-27 03:24:09 +0900

    Feat: String -> Status 변환 컨버터 config

--- backend/src/main/java/com/team6/issue_tracker/config/WebConfig.java
+++ backend/src/main/java/com/team6/issue_tracker/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.team6.issue_tracker.config;
 
+import com.team6.issue_tracker.config.converter.StringToStatusConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToStatusConverter());
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
