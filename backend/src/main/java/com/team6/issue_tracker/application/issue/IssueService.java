c07ecdc9add16c88e221a6f6c0f09cbd204ab80d
saehim <saeilee.vxv@gmail.com>
2023-05-27 22:22:33 +0900

    Refactor: 생성자 어노테이션으로 대체

--- backend/src/main/java/com/team6/issue_tracker/application/issue/IssueService.java
+++ backend/src/main/java/com/team6/issue_tracker/application/issue/IssueService.java
@@ -3,12 +3,15 @@ package com.team6.issue_tracker.application.issue;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class IssueService {
 
     private final IssueRepository issueRepository;
-    public IssueService(IssueRepository issueRepository) {
-        this.issueRepository = issueRepository;
-    }
+
+    private final CommentService commentService;
+    private final MemberService memberService;
+    private final LabelService labelService;
+    private final MilestoneService milestoneService;
 
     public Integer getOpenIssueNum() {
         return issueRepository.countAllByIsDeletedAndIsOpen(false, true);
