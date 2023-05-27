1efd94b10ba08b5d8cfe3c68a6ed0e62b2c4a329
saehim <saeilee.vxv@gmail.com>
2023-05-28 00:07:44 +0900

    Feat: Issue id로 IssueDetail 반환 메서드

--- backend/src/main/java/com/team6/issue_tracker/application/issue/IssueService.java
+++ backend/src/main/java/com/team6/issue_tracker/application/issue/IssueService.java
@@ -1,6 +1,23 @@
 package com.team6.issue_tracker.application.issue;
 
+import com.team6.issue_tracker.application.comment.Comment;
+import com.team6.issue_tracker.application.comment.CommentService;
+import com.team6.issue_tracker.application.comment.dto.CommentDto;
+import com.team6.issue_tracker.application.issue.domain.Issue;
+import com.team6.issue_tracker.application.issue.dto.IssueDetail;
+import com.team6.issue_tracker.application.label.LabelService;
+import com.team6.issue_tracker.application.label.dto.LabelDto;
+import com.team6.issue_tracker.application.member.MemberService;
+import com.team6.issue_tracker.application.member.domain.Member;
+import com.team6.issue_tracker.application.member.dto.MemberDto;
+import com.team6.issue_tracker.application.milestone.Milestone;
+import com.team6.issue_tracker.application.milestone.MilestoneService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -21,4 +38,52 @@ public class IssueService {
         return issueRepository.countAllByIsDeletedAndIsOpen(false, false);
     }
 
+    private Issue findIssueById(Long issueIdx) throws NoSuchElementException {
+        Issue issue = issueRepository.findById(issueIdx).orElseThrow();
+
+        if (issue.getIsDeleted()) {
+            throw new NoSuchElementException("삭제된 이슈입니다.");
+        }
+
+        return issue;
+    }
+
+    public List<Issue> findByfilterWithPage(Integer offset, Integer pageSize, IssueFilter filter) {
+        return issueRepository.findAllBy(
+                filter.getIsOpen(),
+                filter.getMailestone(),
+                filter.getWriter(),
+                filter.getAssignee(),
+                filter.getLabel(),
+                pageSize,
+                offset
+        );
+    }
+
+    public IssueDetail findById(Long issueIdx){
+        Issue issue = findIssueById(issueIdx);
+
+        List<Comment> comments = commentService.getCommentsOnIssue(issueIdx);
+
+        Member writer = memberService.findById(issue.getWriter().getId());
+        Member assignee = memberService.findById(issue.getAssignee().getId());
+
+        Milestone milestone = null;
+        if (issue.getMilestoneIdx() != null) {
+            milestone =milestoneService.findById(issue.getMilestoneIdx().getId());
+        }
+
+        List<LabelDto> labelDtoList = new ArrayList<>();
+        labelService.findAllById(issue.getLabelOnIssue().values())
+                .forEach(l -> labelDtoList.add(LabelDto.of(l)));
+
+        List<CommentDto> commentDtos = new ArrayList<>();
+        for (Comment comment : comments) {
+            Member commentWriter = memberService.findById(comment.getCreatedBy().getId());
+            CommentDto dto = CommentDto.fromComment(comment, commentWriter);
+            commentDtos.add(dto);
+        }
+
+        return IssueMapper.toDetails(issue, MemberDto.from(writer), MemberDto.from(assignee), labelDtoList, milestone, commentDtos);
+    }
 }
