89c327f3b106d7e7c8ae6e35260f3be44307e500
saehim <saeilee.vxv@gmail.com>
2023-05-27 21:54:43 +0900

    Feat: `CommentDto` 생성

--- backend/src/main/java/com/team6/issue_tracker/application/comment/dto/CommentDto.java
+++ backend/src/main/java/com/team6/issue_tracker/application/comment/dto/CommentDto.java
@@ -1,2 +1,30 @@
-package com.team6.issue_tracker.application.comment.dto;public class CommentDto {
+package com.team6.issue_tracker.application.comment.dto;
+
+import com.team6.issue_tracker.application.comment.Comment;
+import com.team6.issue_tracker.application.member.domain.Member;
+import com.team6.issue_tracker.application.member.dto.MemberDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import java.time.Instant;
+
+@Data @Builder
+@AllArgsConstructor
+public class CommentDto {
+
+    private Long commentIdx;
+    private String contents;
+    private MemberDto writer;
+    private Instant createdAt;
+    private Instant editedAt;
+
+    public static CommentDto fromComment(Comment comment, Member writer) {
+        return CommentDto.builder()
+                .commentIdx(comment.getCommentIdx())
+                .contents(comment.getContents().getContents())
+                .createdAt(comment.getCreatedAt())
+                .editedAt(comment.getEditedAt())
+                .writer(MemberDto.from(writer))
+                .build();
+    }
 }
