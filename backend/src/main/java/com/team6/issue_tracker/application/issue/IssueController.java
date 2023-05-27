83b01e2fbacb8baad984298e1bd3a0a497c93956
saehim <saeilee.vxv@gmail.com>
2023-05-27 22:21:16 +0900

    Feat: 이슈 상세 보기 API

--- backend/src/main/java/com/team6/issue_tracker/application/issue/IssueController.java
+++ backend/src/main/java/com/team6/issue_tracker/application/issue/IssueController.java
@@ -1,19 +1,25 @@
 package com.team6.issue_tracker.application.issue;
 
+import com.team6.issue_tracker.application.issue.dto.IssueDetail;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/issue")
+@RequiredArgsConstructor
 public class IssueController {
 
-    @Operation(
-            summary = "",
-            tags = "issue",
-            description = "사용자는 원하는 이슈를 다중 선택하여 상태를 변경할 수 있다."
-    )
-    @PutMapping("")
-    public void updateAllIssueIsOpen() {
+    private final IssueService issueService;
 
+    @Operation(
+            summary = "이슈 상세 보기",
+            tags = "issue",
+            description = "사용자는 이슈 상세 정보를 볼 수 있다."
+    )
+    @GetMapping("/{issueIdx}")
+    public IssueDetail getIssueDetails(@PathVariable("issueIdx") Long issueIdx) {
+        return issueService.findById(issueIdx);
     }
 
     @Operation(
