4591b129a2fbf32e58df8486f97ce9d65f6646c7
saehim <saeilee.vxv@gmail.com>
2023-05-27 21:51:35 +0900

    Feat: `CommentRepository` 이슈 아이디로 코멘트 찾기 메서드
    - 이슈 아이디로 코멘트를 찾아 반환한다.

--- backend/src/main/java/com/team6/issue_tracker/application/comment/CommentRepository.java
+++ backend/src/main/java/com/team6/issue_tracker/application/comment/CommentRepository.java
@@ -1,8 +1,16 @@
 package com.team6.issue_tracker.application.comment;
 
+import com.team6.issue_tracker.application.issue.domain.Issue;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 @Repository
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+public interface CommentRepository extends CrudRepository<Comment, Long>, PagingAndSortingRepository<Comment, Long> {
+
+    List<Comment> findAllByIssueIdx(AggregateReference<Issue, @NotNull Long> issueIdx);
 }
