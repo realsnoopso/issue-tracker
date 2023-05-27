e269565179927b82bbeef8dc97bce8f4d73b53d2
saehim <saeilee.vxv@gmail.com>
2023-05-27 21:53:03 +0900

    Feat: `CommentService` 이슈에 있는 코멘트 찾는 메서드

--- backend/src/main/java/com/team6/issue_tracker/application/comment/CommentService.java
+++ backend/src/main/java/com/team6/issue_tracker/application/comment/CommentService.java
@@ -1,2 +1,19 @@
-package com.team6.issue_tracker.application.comment;public class CommentService {
+package com.team6.issue_tracker.application.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public List<Comment> getCommentsOnIssue(Long issueIdx) {
+        return commentRepository.findAllByIssueIdx(AggregateReference.to(issueIdx));
+    }
+    
 }
