f3d10938a91a95302bd05cf7296da5f647e85699
saehim <saeilee.vxv@gmail.com>
2023-05-28 03:29:40 +0900

    Fix: Milestone 엔티티 빠진부분 수정

--- backend/src/main/java/com/team6/issue_tracker/application/milestone/Milestone.java
+++ backend/src/main/java/com/team6/issue_tracker/application/milestone/Milestone.java
@@ -3,23 +3,19 @@ package com.team6.issue_tracker.application.milestone;
 import lombok.Builder;
 import lombok.Getter;
 import org.joda.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
 
 import java.time.Instant;
 
 @Getter @Builder
 public class Milestone {
-
+    @Id
     private Long milestoneIdx;
-
     private String title;
-
     private Boolean isOpen;
-
     private Instant endedAt;
-
     private String contents;
-
     private Boolean isDeleted;
 
     public Milestone(String title, Instant endedAt, String contents) {
