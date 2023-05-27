02646babeb57ebe1a399da5b242efc31d073cfae
saehim <saeilee.vxv@gmail.com>
2023-05-28 05:10:42 +0900

    Feat: IssueMapper 기능, Dto로 이동 후 Mapper 삭제

--- backend/src/main/java/com/team6/issue_tracker/domain/page/dto/IssueDto.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/page/dto/IssueDto.java
@@ -1,5 +1,6 @@
 package com.team6.issue_tracker.domain.page.dto;
 
+import com.team6.issue_tracker.domain.issue.Issue;
 import com.team6.issue_tracker.domain.model.Status;
 import com.team6.issue_tracker.domain.label.dto.LabelDto;
 import com.team6.issue_tracker.domain.member.dto.MemberDto;
@@ -35,6 +36,20 @@ public class IssueDto {
         this.editedAt = edditedAt;
         this.milestone = milestone;
         this.labelList = labelList;
+    }
+
+    public static IssueDto toDto(Issue issue, MemberDto writer, MemberDto assignee,
+                                 List<LabelDto> labels, Milestone milestone) {
+        return IssueDto.builder()
+                .index(issue.getIssueIdx())
+                .title(issue.getTitle())
+                .writer(writer)
+                .assignee(assignee)
+                .status(Status.of(issue.getIsOpen()))
+                .createdAt(issue.getCreatedAt())
+                .labelList(labels)
+                .milestone(milestone)
+                .build();
     }
 
 }
