ab5a9f91456520060ee98b52c131b1dcf5414552
saehim <saeilee.vxv@gmail.com>
2023-05-28 05:03:19 +0900

    Test: 이슈 내용 수정 테스트

--- backend/src/test/java/com/team6/issue_tracker/issue/IssueRepositoryTest.java
+++ backend/src/test/java/com/team6/issue_tracker/issue/IssueRepositoryTest.java
@@ -1,7 +1,5 @@
-package com.team6.issue_tracker.issue;
+package com.team6.issue_tracker.domain.issue;
 
-import com.team6.issue_tracker.application.issue.IssueRepository;
-import com.team6.issue_tracker.application.issue.domain.Issue;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +10,11 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -71,10 +72,7 @@ class IssueRepositoryTest {
         Page<Issue> issues = issueRepository.findAllByIsDeletedAndIsOpen(false, true, page);
 
         //then
-        SoftAssertions.assertSoftly(softly -> {
-            softly.assertThat(issues).hasSize(1);
-            softly.assertThat(issues.get(0).getTitle()).isEqualTo("test issue");
-        });
+        assertThat(issues).hasSize(1);
     }
 
     @Test
@@ -86,5 +84,61 @@ class IssueRepositoryTest {
 
         //then
         assertThat(allByFilter).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("새로운 이슈를 저장할 수 있다.")
+    public void saveNewIssue() throws Exception {
+        long before = issueRepository.count();
+
+        Issue newIssue = Issue.builder()
+                .writer(AggregateReference.to(1L))
+                .title("새로 저장하는 이슈")
+                .contents("새로 저장하는 이슈의 내용")
+                .createdAt(Instant.now())
+                .isOpen(true)
+                .isDeleted(false)
+                .labelOnIssue(Map.of(1L, new Labeling(1L)))
+                .build();
+
+        Issue save = issueRepository.save(newIssue);
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(save.getIssueIdx()).isNotNull();
+            softly.assertThat(save.getContents()).isEqualTo(newIssue.getContents());
+            softly.assertThat(issueRepository.count()).isEqualTo(before+1);
+        });
+    }
+
+    @Test
+    @DisplayName("이슈 내용을 수정할 수 있다.")
+    public void updateContents() throws Exception{
+        //given
+        Instant beforeWork = Instant.now();
+        Issue issue = issueRepository.findById(1L).orElseThrow();
+        Issue updatedIssue = Issue.builder()
+                .issueIdx(issue.getIssueIdx())
+                .title(issue.getTitle())
+                .contents("수정된 내용입니다")
+                .writer(issue.getWriter())
+                .assignee(issue.getAssignee())
+                .createdAt(issue.getCreatedAt())
+                .milestoneIdx(issue.getMilestoneIdx())
+                .labelOnIssue(issue.getLabelOnIssue())
+                .editedAt(Instant.now())
+                .isOpen(issue.getIsOpen())
+                .isDeleted(issue.getIsDeleted())
+                .build();
+
+        //when
+        Issue save = issueRepository.save(updatedIssue);
+
+        //then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(save.getContents()).isEqualTo(updatedIssue.getContents());
+            softly.assertThat(save.getIssueIdx()).isEqualTo(issue.getIssueIdx());
+            softly.assertThat(save.getCreatedAt()).isEqualTo(issue.getCreatedAt());
+            softly.assertThat(save.getEditedAt()).isAfter(beforeWork);
+        });
     }
 }
