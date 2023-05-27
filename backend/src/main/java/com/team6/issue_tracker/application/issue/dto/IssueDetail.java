3dd921444046ffc14a7edbabbfceec182f0f9f36
saehim <saeilee.vxv@gmail.com>
2023-05-27 22:15:57 +0900

    Feat: `IssueDetail` DTO

---
+++ backend/src/main/java/com/team6/issue_tracker/application/issue/dto/IssueDetail.java
@@ -0,0 +1,31 @@
+package com.team6.issue_tracker.application.issue.dto;
+
+import com.team6.issue_tracker.application.comment.dto.CommentDto;
+import com.team6.issue_tracker.application.label.dto.LabelDto;
+import com.team6.issue_tracker.application.member.dto.MemberDto;
+import com.team6.issue_tracker.application.milestone.Milestone;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.List;
+
+
+@Data @Builder
+@AllArgsConstructor
+public class IssueDetail {
+
+    private Long index;
+    private String title;
+    private MemberDto writer;
+    private MemberDto assignee;
+    private Status status;
+    private Instant createdAt;
+    private Instant editedAt;
+    //TODO milestone dto로 바꾸기
+    private Milestone milestone;
+    private List<LabelDto> labelList;
+    private List<CommentDto> commentList;
+
+}
