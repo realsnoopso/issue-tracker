2db3b65ea26b82b6fbe856fcfb806abe5b74f152
saehim <saeilee.vxv@gmail.com>
2023-05-29 17:03:15 +0900

    Feat: member id로 한꺼번에 멤버 조회

--- backend/src/main/java/com/team6/issue_tracker/domain/member/service/MemberService.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.team6.issue_tracker.domain.member.service;
 
+import com.fasterxml.jackson.databind.util.ArrayIterator;
 import com.team6.issue_tracker.domain.member.domain.Member;
 import com.team6.issue_tracker.domain.member.repository.MemberRepository;
 import com.team6.issue_tracker.domain.member.dto.MemberDto;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -28,5 +30,12 @@ public class MemberService {
         Iterable<Member> member = memberRepository.findAll();
         member.forEach(m -> memberDtos.put(m.getMemberIdx(), MemberDto.from(m)));
         return memberDtos;
+    }
+
+    public Map<Long, Member> findMembers(AggregateReference<Member, Long> writer, AggregateReference<Member, Long> assignee) {
+        Map<Long, Member> memberMap = new HashMap<>();
+        memberRepository.findAllById(new ArrayIterator<>(new Long[]{writer.getId(), assignee.getId()}))
+                .forEach(member -> memberMap.put(member.getMemberIdx(), member));
+        return memberMap;
     }
 }
