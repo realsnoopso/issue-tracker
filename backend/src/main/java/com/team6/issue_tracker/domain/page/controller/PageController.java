7c422629fbcf3b797678264163dfb34e84f0771c
saehim <saeilee.vxv@gmail.com>
2023-05-31 02:29:06 +0900

    Refactor: 사용하지 않는 import 삭제

--- backend/src/main/java/com/team6/issue_tracker/domain/page/controller/PageController.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/page/controller/PageController.java
@@ -5,7 +5,6 @@ import com.team6.issue_tracker.domain.page.dto.IssuePageResponse;
 import com.team6.issue_tracker.domain.page.exception.NoPageSearchElementException;
 import com.team6.issue_tracker.domain.page.service.PageService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
