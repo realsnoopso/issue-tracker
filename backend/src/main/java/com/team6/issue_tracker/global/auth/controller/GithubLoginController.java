639ccdc1929c8671a73562d9feadad41496137fa
lvalentine6 <ysr1227@naver.com>
2023-05-30 14:51:13 +0900

    Feat : jwt 토큰 및 응답 구현

--- backend/src/main/java/com/team6/issue_tracker/global/auth/controller/GithubLoginController.java
+++ backend/src/main/java/com/team6/issue_tracker/global/auth/controller/GithubLoginController.java
@@ -1,14 +1,21 @@
 package com.team6.issue_tracker.global.auth.controller;
 
-import com.team6.issue_tracker.auth.config.GithubOAuthProperties;
-import com.team6.issue_tracker.auth.domain.GithubUser;
-import com.team6.issue_tracker.auth.dto.GithubAccessToken;
-import com.team6.issue_tracker.auth.dto.GithubAccessTokenRequest;
 import com.team6.issue_tracker.domain.member.service.MemberService;
+import com.team6.issue_tracker.global.auth.config.GithubOAuthProperties;
+import com.team6.issue_tracker.global.auth.domain.GithubUser;
+import com.team6.issue_tracker.global.auth.dto.GithubAccessToken;
+import com.team6.issue_tracker.global.auth.dto.GithubAccessTokenRequest;
 import com.team6.issue_tracker.global.auth.service.GithubOAuthService;
+import com.team6.issue_tracker.global.auth.service.JwtService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -21,27 +28,36 @@ public class GithubLoginController {
     private final GithubOAuthProperties githubOAuthProperties;
     private final MemberService memberService;
     private final GithubOAuthService oAuthServices;
+    private final JwtService jwtService;
 
-    public GithubLoginController(GithubOAuthProperties githubOAuthProperties, MemberService memberService, GithubOAuthService oAuthServices) {
+    public GithubLoginController(GithubOAuthProperties githubOAuthProperties, MemberService memberService, GithubOAuthService oAuthServices, JwtService jwtService) {
         this.githubOAuthProperties = githubOAuthProperties;
         this.memberService = memberService;
         this.oAuthServices = oAuthServices;
+        this.jwtService = jwtService;
     }
 
     @GetMapping("/oauth/result")
-    public GithubUser loginViaGithub(String code) {
+    public ResponseEntity<?> loginViaGithub(String code) {
         GithubAccessTokenRequest githubAccessTokenRequest = new GithubAccessTokenRequest(githubOAuthProperties, code);
         GithubAccessToken githubAccessToken = oAuthServices.requestAccessToken(githubAccessTokenRequest);
         GithubUser githubUser = oAuthServices.requestUserInfo(githubAccessToken);
-
         //TODO 로그인 처리
 //        member = loginService.login(member);
 
         //TODO 토큰 jwt 처리
+        String jwtToken = jwtService.createToken(githubUser);
 //        String jwtAccessToken = jwtTokenProvider.issueAccessToken(member);
 
-        //TODO header에 토큰 넣기
-        return githubUser;
+        //TODO header, body에 토큰 넣고 반환
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwtToken);
+
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("message", "login success");
+        responseBody.put("token", jwtToken);
+
+        return new ResponseEntity<>(responseBody, headers, HttpStatus.OK);
     }
 
 }
