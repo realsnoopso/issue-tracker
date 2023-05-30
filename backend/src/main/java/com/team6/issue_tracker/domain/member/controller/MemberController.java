18a1ed1afee84bf0a790b303959ec210ce5a9312
saehim <saeilee.vxv@gmail.com>
2023-05-31 00:08:47 +0900

    Feat: member 목록 조회 매핑

--- backend/src/main/java/com/team6/issue_tracker/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/member/controller/MemberController.java
@@ -1,19 +1,25 @@
 package com.team6.issue_tracker.domain.member.controller;
 
-import com.team6.issue_tracker.domain.member.domain.Member;
-import com.team6.issue_tracker.domain.member.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.team6.issue_tracker.domain.member.dto.MemberDto;
+import com.team6.issue_tracker.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
 
-    private MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    @Autowired
-    public MemberController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    @GetMapping("/members")
+    public List<MemberDto> findAll(int page) {
+        return memberService.getAllMemberPage(page).stream()
+                        .map(MemberDto::from)
+                        .collect(Collectors.toList());
     }
 
 }
