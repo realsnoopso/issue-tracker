45508721e6f214b2ba7aadb591493075dcf50d04
saehim <saeilee.vxv@gmail.com>
2023-05-30 21:17:38 +0900

    Feat: custom Exception 구현
    - page에서 필수 요청이 없을 때

---
+++ backend/src/main/java/com/team6/issue_tracker/domain/page/exception/NoPageSearchElementException.java
@@ -0,0 +1,9 @@
+package com.team6.issue_tracker.domain.page.exception;
+
+public class NoPageSearchElementException extends Exception {
+
+    public NoPageSearchElementException(String message) {
+        super(message);
+    }
+    
+}
