3190f04b8ab86c11a82e665a6810e6e71bdceff7
saehim <saeilee.vxv@gmail.com>
2023-05-30 19:32:02 +0900

    Fix: entity `member` 수정

--- backend/src/main/java/com/team6/issue_tracker/domain/member/domain/Member.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/member/domain/Member.java
@@ -30,19 +30,23 @@ public class Member {
 
     private String profileImageUrl;
 
-    @Column("github_join")
-    private Boolean githubState;
-
     @PersistenceCreator
-    private Member(String id, String name, String password, String profileImageUrl, Boolean githubState) {
+    public Member(Long memberIdx, String id, String name, String password, String profileImageUrl) {
+        this.memberIdx = memberIdx;
         this.id = id;
         this.name = name;
         this.password = password;
         this.profileImageUrl = profileImageUrl;
-        this.githubState = githubState;
+    }
+
+    private Member(String id, String name, String password, String profileImageUrl) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
     }
 
     public static Member newMember(String id, String name, String password, String profileImageUrl) {
-        return new Member(id, name, password, profileImageUrl, false);
+        return new Member(id, name, password, profileImageUrl);
     }
 }
