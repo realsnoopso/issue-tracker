e7e031f5a754c81a1455a38bed5f4e899a18d9e4
saehim <saeilee.vxv@gmail.com>
2023-05-31 00:26:29 +0900

    Docs: 주석 추가

--- backend/src/main/java/com/team6/issue_tracker/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.team6.issue_tracker.domain.member.controller;
 
 import com.team6.issue_tracker.domain.member.dto.MemberDto;
 import com.team6.issue_tracker.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,11 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(
+            summary = "멤버 목록",
+            tags = "members",
+            description = "사용자는 회원 목록을 볼 수 있다."
+    )
     @GetMapping("/members")
     public List<MemberDto> findAll(int page) {
         return memberService.getAllMemberPage(page).stream()
