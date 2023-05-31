d914ecb7ed0b1f86ed852b8cfaf68fd8f29113c2
saehim <saeilee.vxv@gmail.com>
2023-05-31 14:30:02 +0900

    Feat: 이슈 상태 업데이트 service, repository 구현

--- backend/src/main/java/com/team6/issue_tracker/domain/issue/service/IssueUpdateService.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/issue/service/IssueUpdateService.java
@@ -4,10 +4,12 @@ import com.team6.issue_tracker.domain.issue.domain.Issue;
 import com.team6.issue_tracker.domain.issue.repository.IssueRepository;
 import com.team6.issue_tracker.domain.model.Status;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class IssueUpdateService {
@@ -18,7 +20,11 @@ public class IssueUpdateService {
         return issueRepository.save(updatedIssue);
     }
 
-    public boolean updateIssuesStatus(List<Long> issueIdx, Status status) {
+    public boolean updateIssueListStatus(List<Long> issueIdx, Status status) {
         return issueRepository.updateIssuesIsOpen(status == Status.OPEN, issueIdx);
+    }
+
+    public boolean updateIssueStatus(Long index, Status status) {
+        return issueRepository.updateIssueIsOpen(status == Status.OPEN, index);
     }
 }
