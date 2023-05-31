a8e991c3791ed071d56ab26e16717857a7d586ec
saehim <saeilee.vxv@gmail.com>
2023-05-31 14:29:09 +0900

    Fix: Controller DTO 수정

--- backend/src/main/java/com/team6/issue_tracker/domain/issue/controller/IssueController.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/issue/controller/IssueController.java
@@ -3,13 +3,16 @@ package com.team6.issue_tracker.domain.issue.controller;
 import com.team6.issue_tracker.domain.issue.domain.Issue;
 import com.team6.issue_tracker.domain.issue.dto.CreateIssueRequest;
 import com.team6.issue_tracker.domain.issue.dto.IssueDetail;
-import com.team6.issue_tracker.domain.issue.dto.UpdateIssuesStatusRequest;
+import com.team6.issue_tracker.domain.issue.dto.UpdateIssueListStatusRequest;
+import com.team6.issue_tracker.domain.issue.dto.UpdateIssueStatusRequest;
 import com.team6.issue_tracker.domain.issue.service.IssueService;
 import com.team6.issue_tracker.domain.issue.service.IssueUpdateService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class IssueController {
@@ -45,10 +48,10 @@ public class IssueController {
             description = "사용자는 원하는 이슈를 다중 선택해 상태를 변경할 수 있다."
     )
     @PutMapping("/issue")
-    public void updateIssuesStatus(UpdateIssuesStatusRequest request) {
+    public void updateIssuesStatus(UpdateIssueListStatusRequest request) {
         //TODO 유저 권한 검사
         //TODO 유효성 검사
-        issueUpdateService.updateIssuesStatus(request.getIssueIdx(), request.getStatus());
+        issueUpdateService.updateIssueListStatus(request.getIssueIdx(), request.getStatus());
     }
 
     @Operation(
@@ -57,10 +60,10 @@ public class IssueController {
             description = "사용자는 이슈 상태만 수정할 수 있다."
     )
     @PatchMapping("/issue/{issueIdx}/status")
-    public void updateIssuesStatus(IssueDetail request) {
+    public void updateIssuesStatus(@RequestBody UpdateIssueStatusRequest request, @PathVariable("issueIdx") long issueIdx) {
         //TODO 유저 권한 검사
         //TODO 유효성 검사
-        issueUpdateService.updateIssueStatus(request.getIndex(), request.getStatus());
+        issueUpdateService.updateIssueStatus(issueIdx, request.getStatus());
     }
 
     @Operation(
