12caca4c7f8678c0b329578259d91435dfdb841f
lvalentine6 <ysr1227@naver.com>
2023-06-01 21:00:30 +0900

    Feat : ResponseMessage 객체 구현

---
+++ backend/src/main/java/com/team6/issue_tracker/global/util/ResponseMessage.java
@@ -0,0 +1,20 @@
+package com.team6.issue_tracker.global.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Slf4j
+@AllArgsConstructor
+public class ResponseMessage<T> {
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public static <T> ResponseEntity<ResponseMessage<T>> of(HttpStatus status, String message, T data) {
+        return new ResponseEntity<>(new ResponseMessage<>(status, message, data), status);
+    }
+}
