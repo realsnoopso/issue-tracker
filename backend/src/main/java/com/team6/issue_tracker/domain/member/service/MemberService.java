b72b40dd1e096f27110b8040023b65ee495ab565
saehim <saeilee.vxv@gmail.com>
2023-06-02 01:21:49 +0900

    Refactor: 사용하지 않는 코드 정리

--- backend/src/main/java/com/team6/issue_tracker/domain/member/service/MemberService.java
+++ backend/src/main/java/com/team6/issue_tracker/domain/member/service/MemberService.java
@@ -3,12 +3,8 @@ package com.team6.issue_tracker.domain.member.service;
 import com.team6.issue_tracker.domain.member.domain.Member;
 import com.team6.issue_tracker.domain.member.dto.MemberDetail;
 import com.team6.issue_tracker.domain.member.repository.MemberRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.security.auth.login.LoginException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -16,13 +12,12 @@ import java.util.Set;
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
-    private static final Integer PAGE_SIZE = 20;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-    public Member join(Member member) throws LoginException {
+    public Member join(Member member) {
         if (!memberRepository.existsById(member.getId())) {
             return memberRepository.save(member);
         }
@@ -45,12 +40,6 @@ public class MemberService {
         memberRepository.findAllById(members)
                 .forEach(member -> memberMap.put(member.getMemberIdx(), member));
         return memberMap;
-    }
-
-    public Page<Member> getAllMemberPage(int page) {
-        Pageable pageRequest = PageRequest.of(page, PAGE_SIZE);
-//        return memberRepository.findAll(pageRequest);
-        return (Page<Member>) memberRepository.findAll();
     }
 
 }
