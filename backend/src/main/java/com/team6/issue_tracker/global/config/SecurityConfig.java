ec7c6b6f33e8939499fb5347fd76aba110282817
saehim <saeilee.vxv@gmail.com>
2023-06-01 10:53:14 +0900

    Test: 임시로 모든 경로 허용

--- backend/src/main/java/com/team6/issue_tracker/global/config/SecurityConfig.java
+++ backend/src/main/java/com/team6/issue_tracker/global/config/SecurityConfig.java
@@ -20,7 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http    .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/login","/oauth/**",
+                .antMatchers("/**",
+                        "/", "/login","/oauth/**",
                         "/error", "/swagger-ui/**",
                         "/v3/**").permitAll()
                 .anyRequest().authenticated()
