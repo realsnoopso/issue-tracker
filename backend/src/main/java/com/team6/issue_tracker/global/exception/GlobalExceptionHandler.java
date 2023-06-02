96f9f61c07ce1b7f0836ad7b9030c9bf3aa7a146
saehim <saeilee.vxv@gmail.com>
2023-06-02 10:20:42 +0900

    Feat: handleValidationException 추가

--- backend/src/main/java/com/team6/issue_tracker/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/team6/issue_tracker/global/exception/GlobalExceptionHandler.java
@@ -24,6 +24,18 @@ public class GlobalExceptionHandler {
         return ResponseError.of(e, e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
